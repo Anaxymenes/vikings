8fdc92167822b7cedb300071fd58422bd490f6b3
Artur Czopek <artur.czopek@contractors.roche.com>
2018-05-06 23:15:12 +0200

    Little changes

--- login/views.py
+++ login/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html',{})
 
-def register(request)
+def register(request):
     return render(request,'register.html',{})
