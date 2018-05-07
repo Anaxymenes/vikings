1727c7bc08f673e50668cf7d2204c21e4fce979c
Artur Czopek <artur.czopek@contractors.roche.com>
2018-05-07 22:12:22 +0200

    Added hased function

--- models/admin.py
+++ models/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Account, AccountDetails, AccountRole, Achievement, Stage, Task, AchievementTask, Answer, StageStudent, StageTasks, StudentGroup
+
+admin.site.register(Account)
+admin.site.register(AccountDetails)
+admin.site.register(AccountRole)
+admin.site.register(Achievement)
+admin.site.register(Stage)
+admin.site.register(Task)
+admin.site.register(AchievementTask)
+admin.site.register(Answer)
+admin.site.register(StageStudent)
+admin.site.register(StageTasks)
+admin.site.register(StudentGroup)
 
 # Register your models here.
