dc054294e0e1bd81f679bc6e618005593645eefe
Artur Czopek <artur.czopek@contractors.roche.com>
2018-05-07 19:40:00 +0200

    Little fix

--- login/views.py
+++ login/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html',{})
+    return render(request, 'login/index.html',{})
 
 def register(request):
-    return render(request,'register.html',{})
+    return render(request,'login/register.html',{})
