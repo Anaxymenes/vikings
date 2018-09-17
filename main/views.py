14558e42c19c64245bf0ed35cde548bbd9401f01
Anaxymenes <acwmpl@gmail.com>
2018-09-17 23:25:26 +0200

    Add simpla data to base.html

--- main/views.py
+++ main/views.py
@@ -2,14 +2,24 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.models import User
+from models.models import AccountDetails
 
 player="Mateusz Bogacki"
 
 def index(request):
     if request.user.is_authenticated:
-        level=4
+        accountDetails = AccountDetails.objects.filter(user=request.user).first()
+        level = accountDetails.level
         levels = range(1, level + 1)
-        return render(request, 'main/index.html', {"player": request.user.get_full_name(), "levels": levels})
+        exp_bar = (accountDetails.current_exp * 100) / accountDetails.exp_max
+        hp_bar = (accountDetails.current_hp * 100) / accountDetails.hp_max
+        return render(request, 'main/index.html', {
+            "player": request.user.get_full_name(), 
+            "levels": levels, 
+            "account": accountDetails,
+            "exp_bar": exp_bar,
+            "hp_bar": hp_bar
+            })
     return HttpResponseRedirect(reverse('login:login'))
 
 def lesson(request):
--- models/models.py
+++ models/models.py
@@ -1,20 +1,14 @@
 from django.db import models
-
-class Account(models.Model):
-    login = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+from django.contrib.auth.models import User
 
 class AccountDetails(models.Model):
-    account_id = models.ForeignKey(Account , on_delete = models.CASCADE)
+    user = models.ForeignKey(User , on_delete = models.CASCADE)
     hp_max = models.IntegerField(default=100)
     current_hp = models.IntegerField(default=100)
     level = models.IntegerField(default=1)
     points = models.IntegerField(default=0)
     exp_max = models.IntegerField(default=100)
     current_exp = models.IntegerField(default=0)
-
-class AccountRole(models.Model):
-    name = models.CharField(max_length=50)
     
 class Achievement(models.Model):
     name = models.CharField(max_length=50)
@@ -34,16 +28,16 @@ class Task(models.Model):
 class AchievementTask(models.Model):
     achievementId = models.ForeignKey(Achievement, on_delete = models.PROTECT)
     taskId = models.ForeignKey(Task, on_delete=models.CASCADE)
-    studentID = models.ForeignKey(Account, on_delete=models.CASCADE)
+    studentID = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Answer(models.Model):
-    studentId = models.ForeignKey(Account, on_delete = models.CASCADE)
+    studentId = models.ForeignKey(User, on_delete = models.CASCADE)
     taskID = models.ForeignKey(Task, on_delete=models.PROTECT)
     answerSql = models.CharField(max_length=2500)
 
 class StageStudent(models.Model):
     stageId = models.ForeignKey(Stage, on_delete=models.PROTECT)
-    studentID = models.ForeignKey(Account, on_delete=models.CASCADE)
+    studentID = models.ForeignKey(User, on_delete=models.CASCADE)
     databaseSql = models.CharField(max_length=5000)
 
 class StageTasks(models.Model):
@@ -52,7 +46,7 @@ class StageTasks(models.Model):
 
 class StudentGroup(models.Model):
     name = models.CharField(max_length=50)
-    lecturerID = models.ForeignKey(Account, on_delete=models.PROTECT)
+    lecturerID = models.ForeignKey(User, on_delete=models.PROTECT)
 
 
 
