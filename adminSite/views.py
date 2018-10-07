de72705b5e54402cec3c10f21605e739d7def711
Anaxymenes <acwmpl@gmail.com>
2018-10-08 01:36:28 +0200

    fixing analyze xlsx file to adding group

--- adminSite/views.py
+++ adminSite/views.py
@@ -34,9 +34,35 @@ def addGroup(request):
                 rows = wb[sheetname].max_row
                 columns = wb[sheetname].max_column
                 ws = wb[sheetname]
-                for i in range (1, rows+1):
-                    for j in range(1,columns+1):
-                        cell_obj=ws.cell(row=i,column=j)
-                        print(cell_obj.value, end =' | ')
+                firstname_index = 1
+                lastname_index = 1
+                pesel_index = 1
+                index_number_index = 1
+                email_index = 1
+                for i in range(1,columns+1):
+                    cell_value = ws.cell(row=1,column=i).value
+                    if cell_value == "Imię" or cell_value == "Firstname":
+                        firstname_index = i
+                    elif cell_value == "Nazwisko" or cell_value == "Lastname":
+                        lastname_index = i
+                    elif cell_value == "PESEL":
+                        pesel_index = i
+                    elif cell_value == "Numer Indeksu" or cell_value == "Index":
+                        index_number_index = i
+                    elif cell_value == "E-mail" or cell_value == "Email":
+                        email_index = i
+                for i in range (2, rows+1):
+                    password = ws.cell(row=i,column=firstname_index).value [:3]
+                    password += ws.cell(row=i,column=lastname_index).value [:3]
+                    password += str(ws.cell(row=i,column=pesel_index).value) [-3:]
+                    
+                    username = 's'+str(ws.cell(row=i,column=index_number_index).value)
+                    email = str(ws.cell(row=i,column=email_index).value)
+                    print(password)
+                    user = User.objects.create_user(username,email,password)
+                    user.first_name = ws.cell(row=i,column=firstname_index).value
+                    user.last_name = ws.cell(row=i,column=lastname_index).value 
+                    user.save()
+                    
             return render(request, 'admin/excercises.html')
     return render(request, 'admin/groups.html',{'form' : CreateGroup()})
