3d0e9753f0c235328f27444bd13906788597405d
MateuszBogacki <manti.2@vp.pl>
2018-11-11 00:45:09 +0100

    Student rank's

--- main/context_processors.py
+++ main/context_processors.py
@@ -9,10 +9,11 @@ def add_variable_to_context(request):
         accountDetails = AccountDetails.objects.filter(user=request.user).first()
         level = accountDetails.level
         levels = range(1, level + 1)
+        global_ranking = global_rank(request.user)
+        group_ranking = group_rank(request.user)
         openLevels = openStages(request.user)
         exp_bar = (accountDetails.current_exp * 100) / accountDetails.exp_max
         hp_bar = (accountDetails.current_hp * 100) / accountDetails.hp_max
-        print(isNewMessages(request.user))
         return {
             "player": request.user.get_full_name(), 
             "levels": levels, 
@@ -20,7 +21,9 @@ def add_variable_to_context(request):
             "account": accountDetails,
             "exp_bar": exp_bar,
             "hp_bar": hp_bar,
-            "new_msg": isNewMessages(request.user)
+            "new_msg": isNewMessages(request.user),
+            "global_rank": global_ranking,
+            "group_rank": group_ranking
             }
     elif request.user.is_authenticated and request.user.is_superuser :
         return {
@@ -36,3 +39,17 @@ def start_stages(request):
         if stage.end_at >= current_date:
             stage.complete = 1
             stage.save()
+
+def global_rank(user):
+    studentsSortedByPoints = sorted(AccountDetails.objects.all(), key=lambda student: student.points, reverse=True)
+    studentRank = studentsSortedByPoints.index(AccountDetails.objects.filter(user = user).first()) + 1
+    return studentRank 
+
+def group_rank(user):
+    groupMates = StudentGroup.objects.filter(group = StudentGroup.objects.filter(student = user).first().group.id)
+    groupStudents = []
+    for mate in groupMates:
+        groupStudents.append(AccountDetails.objects.filter(user = mate.student).first())
+    groupStudentsSortedByPoints = sorted(groupStudents, key=lambda student: student.points, reverse=True)
+    studentRank = groupStudentsSortedByPoints.index(AccountDetails.objects.filter(user = user).first()) + 1
+    return studentRank
