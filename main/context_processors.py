ccd27b34ed45cb48cdc6890aa14a8e7be3f2eb15
MateuszBogacki <manti.2@vp.pl>
2018-11-10 21:25:23 +0100

    Messages view fix, open stages
    With error ! ! !

--- main/context_processors.py
+++ main/context_processors.py
@@ -2,18 +2,21 @@ from django.contrib.auth.models import User
 from models.models import *
 from datetime import datetime, timedelta, timezone
 from .msgUtil import isNewMessages
+from .stageManagement import *
 
 def add_variable_to_context(request):
     if request.user.is_authenticated and request.user.is_superuser == False: 
         accountDetails = AccountDetails.objects.filter(user=request.user).first()
         level = accountDetails.level
         levels = range(1, level + 1)
+        openLevels = openStages(request.user)
         exp_bar = (accountDetails.current_exp * 100) / accountDetails.exp_max
         hp_bar = (accountDetails.current_hp * 100) / accountDetails.hp_max
         print(isNewMessages(request.user))
         return {
             "player": request.user.get_full_name(), 
             "levels": levels, 
+            "openLevels": openLevels,
             "account": accountDetails,
             "exp_bar": exp_bar,
             "hp_bar": hp_bar,
--- main/stageManagement.py
+++ main/stageManagement.py
@@ -3,3 +3,13 @@ from models.models import *
 from datetime import datetime, timedelta, timezone
 from django.db import transaction
 from django.db.models import Q
+
+def openStages(user):
+    openStages = []
+    stageStudent = StageStudent.objects.filter(student = user).filter(complete = 0)
+    for i in stageStudent:
+        openStages.append({
+            "openLevel": i.stage.id
+        }) 
+        print(i.stage.id)
+    return openStages
