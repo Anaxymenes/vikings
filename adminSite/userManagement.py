3bf994753c49208e917a8a713c462ffbf2a79134
Anaxymenes <acwmpl@gmail.com>
2018-11-12 23:10:05 +0100

    Update adminSite/userManagement.py

--- adminSite/userManagement.py
+++ adminSite/userManagement.py
@@ -28,15 +28,12 @@ def createStudentFromWorkbook(file_in_memory, group):
                 elif cell_value == "Numer Indeksu" or cell_value == "Index" or cell_value == "Nr_Indeksu":
                     index_number_index = i
             for i in range (2, rows+1):
-                password = ws.cell(row=i,column=firstname_index).value [:3]
-                password += ws.cell(row=i,column=lastname_index).value [:3]
-                password += str(ws.cell(row=i,column=index_number_index).value) [-3:]
+                index_nr = str(ws.cell(row=i,column=index_number_index).value)
                 username = 's'+str(ws.cell(row=i,column=index_number_index).value)
                 if not User.objects.filter(username=username).exists():
-                    user = User.objects.create_user(username,None,password)
-                    user.first_name = ws.cell(row=i,column=firstname_index).value
-                    user.last_name = ws.cell(row=i,column=lastname_index).value 
-                    user.save()
+                    first_name = ws.cell(row=i,column=firstname_index).value
+                    last_name = ws.cell(row=i,column=lastname_index).value 
+                    user = createStudent(first_name,last_name,index_nr)
                     studentGroup = StudentGroup.objects.create(group=group, student=user)
                     studentGroup.save()
                     accountDetails = AccountDetails.objects.create(hp_max=100,current_hp=100,level=0,points=0,exp_max=100,current_exp=0,user=user)
